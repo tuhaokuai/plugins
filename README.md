6118c160a4054ac1969601234010476ae502b3a5
TuHaoKuai <seash.huang@snow021.com>
2015-11-18 15:16:29 +0800

    discuz7.2 帖子图片更换，加速网站访问

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# plugins
-全站加速插件,支持discuz,discuzX,wordpress,drupal......
+# 图好快 官方插件
+
+全站加速插件
+
+
+目前已完成discuz7.2 简单中文 utf8。
+
+## DISCUZ 7.X 使用方法：
+
+1. 下载discuz7.2 到您的网站plugins下并改名为tuhaokuai
+
+2. 进入后台->插件->导入 选择导入 plugins/tuhaokuai/discuz_plugin_tuhaokuai.xml  提交。
+
+3. 在插件列表中启用tuhaokuai这个插件。
+
+功能说明：
+
+目前仅对帖子中的图片提供加速功能,以及发帖回帖人的头像为静态地址(如/data...jpg png gif，不支持以.php生成的地址)
+
+
---
+++ discuz7.2/plugin.class.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * 图好快提供的全站图片加速服务
+ *
+ * @since discuz 7.2
+ * @link http://www.tuhaokuai.com
+ * @copyright 上海枫雪信息科技有限公司 
+ * 
+ */
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+if($_SERVER['SCRIPT_NAME'] == '/admincp.php'){
+	return;
+}
+
+ 
+class plugin_tuhaokuai {
+	public $url = "http://s1.tuhaokuai.com";
+	 
+	function viewthread_postheader_output(){
+		global  $postlist;
+		foreach($postlist  as $k=>$v){
+			unset($c,$arr);
+			if(array_key_exists('message',$v) && $v['message']){
+				$c = $v['message'];
+				$arr = $this->image($c);
+				$arr1 = $this->image($c,'file');
+				if($arr){
+					foreach ($arr as $ui){
+						$c = str_replace($ui,$this->link($ui),$c);
+					}
+				}
+				if($arr1){
+					foreach ($arr1 as $ui){
+						$c = str_replace($ui,$this->link($ui),$c);
+					}
+				}
+				$postlist[$k]['message'] = $v['message'] = $c;
+			}
+			
+			if(array_key_exists('avatar',$v) && $v['avatar']){
+				$c = $v['avatar'];
+				$arr = $this->image($c);
+				$ui = $arr[0];
+				if(strpos($ui,'.php')===false){
+					$postlist[$k]['avatar'] = str_replace($ui,$this->link($ui),$c);
+				}
+			}
+			
+			 
+			
+		}
+		
+	 
+		
+	}
+	
+	
+	/**
+	 * 
+	 * create new link
+	 * @param $url
+	 */
+	function link($url){
+		
+		$ext = substr($url,strrpos($url,'.'));
+		
+		$arr = [
+			'jpg','png','jpeg','bmp','gif','js','css'
+		];
+		 
+		$host = $_SERVER['HTTP_HOST'];
+		$a = 'http://'.$host;
+		$b = 'https://'.$host;
+		if(strpos($url,$a) === false){
+			return $this->url.'/'.substr($a,7).'/'.$url;
+		}else{
+			return str_replace($a,$this->url.'/'.$host,$url);
+		}
+		if(strpos($url,$b) === false){
+			return $this->url.'/'.substr($b,8).'/'.$url;
+		}else{
+			return str_replace($a,$this->url.'/'.$host,$url);
+		}
+		
+		return  $url;
+	}
+	
+	/**
+	*  get image url
+	*/
+	function image($content,$tag = 'src'){ 
+		$preg = "/<\s*img\s+[^>]*?$tag\s*=\s*(\'|\")(.*?)\\1[^>]*?\/?\s*>/i";
+		preg_match_all($preg,$content,$out);
+		return $out[2];  
+	}
+	
+     
+
+}
+
+
+ 
+
