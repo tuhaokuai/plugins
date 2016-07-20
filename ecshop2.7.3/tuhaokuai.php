1946bdbd5f34d3fb8e9de35095fb64a527a77b7d
TuHaoKuai <seash.huang@snow021.com>
2016-07-20 18:16:08 +0800

    ecshop 有手机站 mobile 特殊处理
    ecshop 有手机站 mobile 特殊处理

--- ecshop2.7.3/tuhaokuai.php
+++ ecshop2.7.3/tuhaokuai.php
@@ -15,8 +15,10 @@ echo $tu->linkNew('http://a/baidu.jpg')."<br>";
 */
 class tuhaokuai {
     public $url = "http://s1.tuhaokuai.com";
-    public $useJsLink = true;
-    public $useCssLink = true;
+    public $open = true;//是否开启加速域名
+    public $fixPort = true;
+    public $useJsLink = false;
+    public $useCssLink = false;
     public $useImageLink = true;
     public $useHrefLink = false;
     public $https = false;
@@ -29,6 +31,9 @@ class tuhaokuai {
             'googleapis.com'
     );
     function output($string){
+         if($this->open !== true){
+            return $string;
+         }
          if(strpos( $_SERVER['SCRIPT_NAME'] ,'/admin/')!==false ){
             return $string;
          }
@@ -44,6 +49,8 @@ class tuhaokuai {
          if($this->useJsLink === true){
              $string = $this->replace('script',$string);
          }
+
+         $string = $this->replace_script_ecshop($string);
          return $string;
     }
     
@@ -61,34 +68,66 @@ class tuhaokuai {
                         continue;
                     }
                 } 
-                if(!static::$only[md5($v)]){
+                if(!self::$only[md5($v)]){
                     $string = str_replace($v,$this->linkNew($v),$string);
-                    static::$only[md5($v)] = true;
+                    self::$only[md5($v)] = true;
                 }
             }
          } 
          return $string; 
     }
     
+
+    function replace_script_ecshop($string){
+        if(strpos($_SERVER['REQUEST_URI'],'mobile')!==false){
+            $host = $_SERVER['HTTP_HOST'];
+            preg_match("/img1=new Image\(\);img1\.src='(.*)'/", $string, $matches);
+            if($matches[0] && $matches[1]){
+                $cdn_img = $matches[1];
+                $cdn_img = '/'.str_replace('./../','',$cdn_img);
+
+                $new = "img1=new Image();img1.src='".$this->url.'/'.$host.$cdn_img."'";
+                $string = str_replace($matches[0],$new,$string);
+            }
+            
+
+            
+        }
+        return $string;
+    }
+
+    
+   
+    
+    function tryFixPort($url){
+        if(strpos($_SERVER['REQUEST_URI'],'mobile')!==false){
+            if(strpos($url,'./../')!==false){
+                $url = str_replace('./../','',$url);
+            }
+            
+        }
+        return $url;
+    }
+
+
     /**
      * 
      * create new link
      * @param $url
      */
     function linkNew($url){ 
-
-
         if(strpos($url,$this->url)!==false){
             return $url;
         }
+        
         $key = 'tuhaokuailink'.md5($url);
-        if(isset(static::$NoRepeat[$key])){
-            return  static::$NoRepeat[$key];
+        if(isset(self::$NoRepeat[$key])){
+            return  self::$NoRepeat[$key];
         }
         $ext = substr($url,strrpos($url,'.')+1); 
         
         if(!in_array($ext,$this->allowExt) && !$this->allowDomain($url)){
-            static::$NoRepeat[$key] = $url;
+            self::$NoRepeat[$key] = $url;
             return  $url;
         }
         $host = $_SERVER['HTTP_HOST'];
@@ -96,47 +135,45 @@ class tuhaokuai {
         if($this->https === true){
             $top = 'https:';
         }  
-
         //对URL分析
         // http://yourdomain/a/b/c.jpg
         // return  http://s1.tuhaokuai.com/yourdomain/a/b/c.jpg
         if(strpos($url,$top.'//'.$host)!==false){ 
             $url = $this->url.'/'.substr($url,strlen($top."//"));
-            static::$NoRepeat[$key] = $url;
+            self::$NoRepeat[$key] = $url;
             return  $url;
         }
-
         // http://notyourdomain/a/b/c.jpg
         // return  http://s1.tuhaokuai.com/yourdomain/http://notyourdomain/a/b/c.jpg
         
         if(substr($url,0,7)=='http://'){
             $url = "/".$host."/".$url;
             $url = $this->url.$url;
-            static::$NoRepeat[$key] = $url;
+            self::$NoRepeat[$key] = $url;
             return  $url;
         }
-
         if(substr($url,0,2)=='//'){
             $url = "/".$host."/http:".$url;
             $url = $this->url.$url;
-            static::$NoRepeat[$key] = $url;
+            self::$NoRepeat[$key] = $url;
             return  $url;
         }
-
-
         if(substr($url,0,1)=='/'){
             $url = "/".$host.$url;
             $url = $this->url.$url;
-            static::$NoRepeat[$key] = $url;
+            self::$NoRepeat[$key] = $url;
             return  $url;
-
         }
- 
+     
+         //处理../../目录
         
-        return $url;
-
-
-
+        if($this->fixPort === true && strpos($url,'../')!==false){
+             
+            $url = $this->tryFixPort($url);
+            
+        }
+        
+        return $this->url.'/'.$host.'/'.$url;
         
         
     }
